5997f42fb45a96ec6a43d5947075acd11f286449
Коля <Y98ojun1@yandex.ru>
2020-01-11 22:19:25 +0300

    .

--- Main.py
+++ Main.py
@@ -1,29 +1,16 @@
 import os
-
 import pygame
-
 from Constants import *
 from Player import *
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    image = pygame.image.load(fullname).convert_alpha()
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-class Main:
+class Main():
     def __init__(self, screen):
         self.screen = screen
         self.running = True
         self.background = pygame.image.load('data/ClubNeon.png')
         self.main_loop()
+        self.player = Player
 
     def handle_events(self):
         for event in pygame.event.get():
---
+++ groups.py
@@ -0,0 +1,5 @@
+import pygame
+
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
