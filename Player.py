7f78aa20400f0d56df65e201381990416c88179f
Коля <Y98ojun1@yandex.ru>
2020-01-11 22:20:10 +0300

    .

--- Player.py
+++ Player.py
@@ -1,15 +1,19 @@
 import pygame
 from Constants import *
+from groups import *
 
 
-class Player:
-    def __init__(self, name):
-
+class Player(pygame.sprite.Sprite):
+    def __init__(self, name, player_image):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
         self.name = name
         self.state = ALIVE
         self.x = START_X
         self.y = START_Y
         self.hp = MAX_HP
+        pos_x = pos_y = 0
+        self.rect = self.image.get_rect().move(0, 0)
 
     def move(self):
         """ the movement of the player """
