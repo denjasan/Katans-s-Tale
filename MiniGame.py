ad10e536c712c7051b16984b45e736fd9c2c5453
Коля <Y98ojun1@yandex.ru>
2020-01-12 21:35:37 +0300

    .

--- MiniGame.py
+++ MiniGame.py
@@ -1,33 +1,24 @@
+import os
 import pygame
-
 from Constants import *
+from groups import *
+
+size = width, height = 800, 600
 
 
-class MiniGame:
-    def __init__(self, enemy, hp):
+class Heart(pygame.sprite.Sprite):
+    def __init__(self, group, image):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно!!!
+        super().__init__(group)
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = width // 2
+        self.rect.y = height // 2
 
-        self.hp = hp
-        self.enemy = enemy
-        self.status = BATTLE
-        self.player = Heart()
+    def update(self, x, y):
+        if self.rect.x == 0 and x > 0 or self.rect.x == width and x < 0:
+            self.rect.x += x
 
-    def render(self):
-        pass
-
-    def main_loop(self):
-        pass
-
-
-class Heart:
-    def __init__(self):
-        self.image =
-        self.x =
-        self.y =
-
-    def move(self):
-
-
-
-
-
-
+        if self.rect.y == 0 and y > 0 or self.rect.y == height and y < 0:
+            self.rect.y += y
