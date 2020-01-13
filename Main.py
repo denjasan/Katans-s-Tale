dfbfe30c601e7eeb3901213cea375db7ee639473
denjasan <denjasan@users.noreply.github.com>
2020-01-13 21:23:40 +0300

    Если ты что-то потерял, то я ничего не удалял, мог закоментировать или перенести куда-нибудь

--- Main.py
+++ Main.py
@@ -6,43 +6,63 @@ from functions import *
 from game_area import *
 
 
+def all_pics(path, n):
+    images = []
+    for i in range(n):
+        image = load_image(path + str(i) + '.gif')
+        images.append(load_image(image))
+    return images
+
+
 class Main():
     def __init__(self, screen):
         self.screen = screen
         self.running = True
         self.background = pygame.image.load('data/ClubNeon.png')
-        self.player = Player
-        self.image = pygame.image.load('data/Zero/Run/0.gif')
+        self.player = Player('Sosiska')
+        self.image = pygame.image.load('data/Zero/Stand/0.gif')
         self.image = pygame.transform.scale(self.image, (60, 50))
         self.area = Area()
         self.x = self.y = 0
+        self.main_loop()
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-        keys = list(pygame.key.get_pressed())
-        if 1 in keys:
-            if keys[97] == 1:
-                self.x = -1
-            if keys[100] == 1:
-                self.x = 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    self.player.mooving = [0, 1]
+                if event.key == pygame.K_a:
+                    self.player.mooving = [1, 0]
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    self.player.mooving = [0, 0]
+                if event.key == pygame.K_a:
+                    self.player.mooving = [0, 0]
+        # keys = list(pygame.key.get_pressed())
+        # if 1 in keys:
+        #     if keys[97] == 1:
+        #         self.x = -1
+        #     if keys[100] == 1:
+        #         self.x = 1
 
     def render(self):
         """ rendering everything """
-        self.screen.blit(self.background, (0, 0))
 
-        player_group.update(self.x, self.area)
-        self.x = 0
-        player_group.draw(self.screen)
+        self.player.render(self.screen, self.area)
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
 
     def main_loop(self):
         """ main program cycle """
-        new_player = Player("Main player", self.image)
+        # new_player = Player("Main player", self.image)
         while self.running:
-            self.handle_events()
+            if self.player.state != DEAD:
+                self.player.moove()
             self.render()
+            self.handle_events()
 
 
 if __name__ == '__main__':
--- Player.py
+++ Player.py
@@ -4,28 +4,43 @@ from groups import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, name, player_image):
+    def __init__(self, name):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
+        # self.image = player_image
         self.name = name
         self.state = ALIVE
         self.x = START_X
         self.y = START_Y
         self.hp = MAX_HP
+        self.direction = RIGHT
+        self.mooving = [0, 0]
+
+        self.run = all_sprites()
+
         pos_x = pos_y = 0
-        self.rect = self.image.get_rect().move(100, 375)
+        # self.rect = self.image.get_rect().move(100, 375)
 
     def update(self, x, mountain):
-        self.rect.x += x
-        if not pygame.sprite.collide_mask(self, mountain):
-            self.rect = self.rect.move(0, 1)
-    def move(self):
-        """ the movement of the player """
         pass
+        # self.rect.x += x
+        # if not pygame.sprite.collide_mask(self, mountain):
+        #     self.rect = self.rect.move(0, 1)
 
-    def render(self):
+    def moove(self):
+        """ the movement of the player """
+        if self.mooving[RIGHT] == 1:
+            self.direction = RIGHT
+            self.x += PLAYER_SPEED
+        if self.mooving[LEFT] == 1:
+            self.direction = LEFT
+            self.x -= PLAYER_SPEED
+
+    def render(self, screen, area):
         """ rendering player """
-        pass
+        screen.blit(self.i)
+        # player_group.update(self.x, area)
+        # self.x = 0
+        # player_group.draw(screen)
 
 
 
