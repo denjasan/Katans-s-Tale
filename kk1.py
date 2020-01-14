da613b82211a1f62982898e5990c65341905c5b4
Коля <Y98ojun1@yandex.ru>
2020-01-14 16:52:14 +0300

    .

--- kk1.py
+++ kk1.py
@@ -2,12 +2,12 @@ import os
 import random
 import pygame
 
-
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 pygame.display.flip()
-all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -29,60 +29,74 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface([20, 20])
         self.image.fill(pygame.Color("blue"))
         self.rect = self.image.get_rect()
-        self.sleep = False
+        self.rect.x = width // 2
+        self.rect.y = height // 2
 
-    def update(self, n):
-        if not self.sleep:
-            self.rect.y = n
-        if pygame.sprite.spritecollideany(self, all_obstacles):
-            self.sleep = True
-        else:
-            self.sleep = False
+    def update(self, x, y):
+        self.rect.x += x
+        if self.rect.x > width:
+            self.rect.x -= width
+        if self.rect.x < 0:
+            self.rect.x += width
+
+        self.rect.y += y
+
+        if self.rect.y > height:
+            self.rect.y -= height
+        if self.rect.y < 0:
+            self.rect.y += height
 
 
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, group, x, y):
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, group):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно!!!
         super().__init__(group)
-        self.image = pygame.Surface([50, 10])
-        self.image.fill(pygame.Color("grey"))
-        self.rect = pygame.Rect(x, y, 50, 10)
+        self.image = pygame.Surface([20, 20])
+        self.image.fill(pygame.Color("white"))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, width)
+        self.vx = self.vy = 7
+        self.rect.y = random.randint(0, height)
+
+    def update(self):
+        self.rect.x += self.vx
+        if self.rect.x > width or self.rect.x < 0:
+            self.rect.x = random.randint(0, width)
+
+        self.rect.y += self.vy
+
+        if self.rect.y > height or self.rect.y < 0:
+            self.rect.y = random.randint(0, height)
 
 
-all_obstacles = pygame.sprite.Group()
+heart = Player(player_group)
 running = True
 y = 0
-v = 50  # пикселей в секунду
+v = 300  # пикселей в секунду
 fps = 60
+dx = dy = 0
 clock = pygame.time.Clock()
+for _ in range(10):
+    Enemy(enemy_group)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                Platform(all_obstacles, event.pos[0], event.pos[1])
-            if event.button == 3 and not all_sprites:
-                Player(all_sprites)
-                for i in all_sprites:
-                    i.rect.x = event.pos[0]
-                    i.rect.y = event.pos[1]
-                y = event.pos[1]
-            elif event.button == 3 and all_sprites:
-                for i in all_sprites:
-                    i.rect.x, i.rect.y = event.pos
-                    y = event.pos[1]
+
     if pygame.key.get_pressed()[275]:
-        for i in all_sprites:
-            i.rect.x += 10
+        dx = v / fps + 1
     if pygame.key.get_pressed()[276]:
-        for i in all_sprites:
-            i.rect.x -= 10
+        dx = -v / fps
+
+    if pygame.key.get_pressed()[274]:
+        dy = v / fps + 1
+    if pygame.key.get_pressed()[273]:
+        dy = -v / fps
     screen.fill((0, 0, 0))
-    if all_sprites:
-        y += v / fps
-    all_sprites.draw(screen)
-    all_obstacles.draw(screen)
-    all_sprites.update(y)
+    heart.update(dx, dy)
+    enemy_group.update()
+    enemy_group.draw(screen)
+    dx = dy = 0
+    player_group.draw(screen)
     clock.tick(fps)
     pygame.display.flip()
